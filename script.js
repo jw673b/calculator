d2410f52a107e3de16c9ecc6c01a5ab62efc0c39
Jia Wang <jiahuiwang.671@gmail.com>
2020-08-11 23:39:36 -0400

    Added capabilities for calculator to take inputs. Must debug dotCheck() function to not allow multiple dots in the first input number

--- script.js
+++ script.js
@@ -7,32 +7,90 @@ for (let i=0;i<10;i++) {
     btn.className = "numBtn";
     btn.innerHTML = `${i}`;
     inputs.appendChild(btn);
+
 }
-//intializes calculator
+//Code to intialize calculator
 const startCalc = function() {
-    
-    //creates array from all div "buttons"
-    const buttons = document.querySelectorAll(".inputBtn, .numBtn");
+    const buttons = document.querySelectorAll(".inputBtn, .numBtn, .equalsBtn, .signBtn");
     const btnsArray = Array.from(buttons);
-    //sets button positions
+    const numBtns = document.querySelectorAll(".numBtn");
+    const numBtnsArray = Array.from(numBtns);
+    const inputBtns = document.querySelectorAll(".inputBtn");
+    const inputBtnsArray = Array.from(inputBtns);
+    const equalsBtn = document.querySelector(".equalsBtn");
+    const signBtn = document.querySelector(".signBtn");
     btnsArray.forEach(btn => {
         btn.style.gridArea =`${btn.id}`;
-    setEventListeners(btnsArray);
-});
+    });
+    addArrayListener(numBtnsArray, addInput);
+    addArrayListener(inputBtnsArray, addInput);
+    equalsBtn.addEventListener('click',eval);
+    //update code for changeSigns to allow for negative numbers
+    //signBtn.addEventListener('click', changeSigns);
 }
-startCalc();
-
-
-function setEventListeners(array) {
+//code to add event listener to all nodes in an array
+function addArrayListener(array, func) {
     array.forEach(btn => {
-        btn.addEventListener("click", addInput);
+        btn.addEventListener("click", func);
     });
 }
+//adds elements to the calculator textbox (allows you to write 2 + 2)
 function addInput(e) {
-    //no code is executed if the first button pressed is not a number button
-    if (inputTextBox.value === "" && e.target.className === "inputBtn") {}
+    const val = inputTextBox.value;
+    //checks if the first button pressed is not a number button
+    if (val === "" && e.target.className === "inputBtn" ||
+     //checks if the last input was a math function and the next button is another math function
+        checkPrevInput(val[val.length-2],["&","+","-","÷","x","^"]) 
+        && e.target.className === "inputBtn" ||
+    //prevents illogical use of % or .
+        val[val.length-1] === "%" &&
+        checkPrevInput(e.target.innerHTML,["%",".",0,1,2,3,4,5,6,7,8,9]) ||
+        val[val.length-1] === "." &&
+        checkPrevInput(e.target.innerHTML,["%","."]) ||
+        dotCheck() &&
+        e.target.innerHTML === ".") 
+        {
+        console.log("if");
+    }
+    //code if false
     else {
         inputTextBox.value += e.target.innerHTML;
-        console.log(inputTextBox.value.length);
+        console.log("else");
     }
 }
+//checks value against array values and returns true if it exists in the array
+const checkPrevInput = function(value,array) {
+    for (let i=0;i<array.length;i++) {
+        if (value == array[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+//change number negative or positive - pending completion
+function changeSigns() {
+    if (checkPrevInput(inputTextBox.value[inputTextBox.value.length - 1],["%",0,1,2,3,4,5,6,7,8,9])) {
+        console.log(inputTextBox.value.lastIndexOf(" "));
+    }
+}
+function dotCheck() {
+    const val = inputTextBox.value;
+    if (val.lastIndexOf(" ") === -1 
+        && val.lastIndexOf(".") === -1) {
+        return false;
+        }
+    else if (val.lastIndexOf(" ") === -1) {
+        console.log(val.lastIndexOf("."));
+        console.log(val.lastIndexOf(".") === -1);
+        return val.lastIndexOf(".") === -1;
+    } else {
+        return val.lastIndexOf(".") > val.lastIndexOf(" ");
+    }
+}
+//evaluate the calculator inputs
+function eval() {
+    let expression = inputTextBox.value.split(" ").join("");
+    console.log(expression);
+}
+//Initializes calculator
+startCalc();
