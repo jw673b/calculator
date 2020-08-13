852e189fe9086ebcdef65a0932a18fc25b698bbf
Jia Wang <jiahuiwang.671@gmail.com>
2020-08-12 23:58:58 -0400

    Added evalPercent and evalExponent functions to script.js. evalExponent crashes browser when there is more than 1 exponent in an expression

--- script.js
+++ script.js
@@ -38,24 +38,19 @@ function addArrayListener(array, func) {
 function addInput(e) {
     const val = inputTextBox.value;
     //checks if the first button pressed is not a number button
-    if (val === "" && e.target.className === "inputBtn" ||
+    if (val === "" && e.target.className !== "numBtn" ||
      //checks if the last input was a math function and the next button is another math function
         checkPrevInput(val[val.length-2],["&","+","-","÷","x","^"]) 
         && e.target.className === "inputBtn" ||
-    //prevents illogical use of % or .
+    //prevents illogical use of %
         val[val.length-1] === "%" &&
         checkPrevInput(e.target.innerHTML,["%",".",0,1,2,3,4,5,6,7,8,9]) ||
-        val[val.length-1] === "." &&
-        checkPrevInput(e.target.innerHTML,["%","."]) ||
-        dotCheck() &&
-        e.target.innerHTML === ".") 
-        {
-        console.log("if");
-    }
-    //code if false
+    //prevents illogical use of .
+        checkPrevInput(e.target.innerHTML, checkPrevNum()) &&
+        checkPrevInput(e.target.innerHTML,["%","."])
+        ) {/*code if true*/}
     else {
         inputTextBox.value += e.target.innerHTML;
-        console.log("else");
     }
 }
 //checks value against array values and returns true if it exists in the array
@@ -67,30 +62,69 @@ const checkPrevInput = function(value,array) {
     }
     return false;
 }
+function checkPrevNum() {
+    if (inputTextBox.value.lastIndexOf(" ") === -1) {
+        return inputTextBox.value.split("");
+    } else {
+        const subStr = 
+        inputTextBox.value.substring(inputTextBox.value.lastIndexOf(" "),inputTextBox.value.length);
+        return subStr.split("");
+    }
+}
 //change number negative or positive - pending completion
 function changeSigns() {
     if (checkPrevInput(inputTextBox.value[inputTextBox.value.length - 1],["%",0,1,2,3,4,5,6,7,8,9])) {
         console.log(inputTextBox.value.lastIndexOf(" "));
     }
 }
-function dotCheck() {
-    const val = inputTextBox.value;
-    if (val.lastIndexOf(" ") === -1 
-        && val.lastIndexOf(".") === -1) {
-        return false;
-        }
-    else if (val.lastIndexOf(" ") === -1) {
-        console.log(val.lastIndexOf("."));
-        console.log(val.lastIndexOf(".") === -1);
-        return val.lastIndexOf(".") === -1;
-    } else {
-        return val.lastIndexOf(".") > val.lastIndexOf(" ");
-    }
-}
 //evaluate the calculator inputs
 function eval() {
-    let expression = inputTextBox.value.split(" ").join("");
-    console.log(expression);
+    let expArr = inputTextBox.value.replace(" ^ ","^").split(/ ([+-x÷]) /g);
+    evalPercents(expArr);
+    console.log(expArr);
+    evalExponents(expArr);
+    console.log(expArr);
+}
+function evalExponents(expArr) {
+    for (let i=0;i<expArr.length;i++) {
+        let val = expArr[i];
+        if (val.indexOf("^") !== -1) {
+            let valArr = val.split("^");
+            let num = 0;
+            let exp;
+            for (i=valArr.length-1;i>0;i--) {
+                exp = valArr[i];
+                num === 0 ? 
+                    num = valArr[i-1]:
+                    num = num;
+                num = num**exp;
+            }
+            expArr[i] = num;
+        } else {}
+    }
+}
+function evalPercents(expArr) {
+    for (let i=0;i<expArr.length;i++) {
+        let val = expArr[i];
+        //if exponents and percents exist in a value 
+        if (val.indexOf("%") !== -1 && val.indexOf("^") !== -1) {
+            val = val.split(/([\^])/);
+            for (let i=0;i<val.length;i++) {
+                if (val[i].indexOf("%") !== -1) {
+                    val[i] = val[i].slice(0,val[i].length-1);
+                    val[i] = val[i]/100;
+                } else {
+                    val[i] = val[i];
+                }
+            }
+            expArr[i] = val.join("");
+        //if only percents exist in  value
+        } else if (val.indexOf("%") !== -1) {
+            val = val.slice(0,val.length-1);
+            val = val/100;
+            expArr[i] = val;
+        } else {}
+    }
 }
 //Initializes calculator
 startCalc();
